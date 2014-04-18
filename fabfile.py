10fe49ea64b117b14c3e09a4c508ef7b44828f85
ckcollab <eric@ckcollab.com>
2014-04-18 15:53:02 -0700

    awesome fresh_db working!

--- fabfile.py
+++ fabfile.py
@@ -1,11 +1,30 @@
-from fabric.api import local
+import sys
+from fabric.api import local, hide
 
 def deploy():
     local("git push")
     local("git push heroku master")
 
 def fresh_db():
-    # destroy db
-    # python manage.py syncdb --noinput
-    # python manage.py migrate
-    pass
+    print "%" * 80
+    print " Starting from scratch!"
+    print "%" * 80
+    print ""
+
+    with hide('running', 'stdout'):
+        sys.stdout.write("Dropping database...")
+        local('psql -U postgres -c "DROP DATABASE IF EXISTS personalrecord"')
+        print "dropped"
+
+        sys.stdout.write("Creating database...")
+        local('psql -U postgres -c "CREATE DATABASE personalrecord"')
+        print "database created"
+
+        sys.stdout.write("Syncdb and migrate...")
+        local('python manage.py syncdb --noinput')
+        local('python manage.py migrate')
+        print "sync'd and migrated"
+
+        sys.stdout.write("Initializing facebook and admin...")
+        local('python manage.py init')
+        print "initialized"
