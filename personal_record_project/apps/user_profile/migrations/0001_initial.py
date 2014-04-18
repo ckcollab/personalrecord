6a94155ca058b697570604efaf653dfd3d53da4b
ckcollab <eric@ckcollab.com>
2014-04-18 15:30:48 -0700

    depends on may fix heroku?

--- personal_record_project/apps/user_profile/migrations/0001_initial.py
+++ personal_record_project/apps/user_profile/migrations/0001_initial.py
@@ -6,6 +6,9 @@ from django.db import models
 
 
 class Migration(SchemaMigration):
+    depends_on = (
+        ("allauth", "0001")
+    )
 
     def forwards(self, orm):
         # Adding model 'UserProfile'
