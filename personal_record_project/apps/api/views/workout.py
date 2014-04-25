7c77c8809c08ae6617238e3b1a51520fd6033c0f
ckcollab <eric@ckcollab.com>
2014-04-25 03:37:23 -0700

    workout permissions done

--- personal_record_project/apps/api/views/workout.py
+++ personal_record_project/apps/api/views/workout.py
@@ -1,4 +1,5 @@
 from django.core.exceptions import ObjectDoesNotExist
+from django.shortcuts import get_object_or_404
 
 from rest_framework import mixins
 from rest_framework import permissions
@@ -8,20 +9,27 @@ from rest_framework.response import Response
 
 from personal_record_project.apps.workout.models import Workout
 
+from api.permissions import IsOwnerOrReadOnly
 from api.serializers.workout import WorkoutSerializer
 
 
 class WorkoutListView(generics.CreateAPIView, generics.ListAPIView, generics.GenericAPIView):
     queryset = Workout.objects.all()
     serializer_class = WorkoutSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
 
 class WorkoutDetailView(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, generics.GenericAPIView):
     queryset = Workout.objects.all()
     serializer_class = WorkoutSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
 
     def get(self, request, *args, **kwargs):
         return self.retrieve(request, *args, **kwargs)
 
     def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs);
+        print args
+        print kwargs
+        obj = get_object_or_404(Workout, **kwargs)
+        self.check_object_permissions(request, obj)
+        return self.update(request, *args, **kwargs)
